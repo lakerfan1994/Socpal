d9f1380dfb6b5d95d43c76ffea045c42d4e6a97b
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2018-07-05 11:18:39 -0400

    added API functionality

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const todayMatchApi = "https://worldcup.sfg.io/matches/today";
+const todayMatchApi = "https://worldcup.sfg.io/matches";
+const flagUrl = 'https://restcountries.eu/rest/v2/alpha/';
 
 function showCurrentGames(){
   $('.current-game-button').click(function(){
-    
+    console.log("print");
+    $('header').addClass("hidden");
+    $('.current-game').removeClass('hidden');
+    getSoccerApi(updateInfo);
   })
 }
 
-function generateCurrentMatch() {
-
-}
 
 function getSoccerApi(callback) {
   const params = {url: todayMatchApi, data: {by_date: "desc"}, success: callback }
@@ -17,15 +18,48 @@ function getSoccerApi(callback) {
 
 function updateInfo(data){
   let dataApi = data;
-  let giantList = dataApi.map()
-  
+ const currentMatch = data.find(findMostRecentGame);
+ const homeFlagId = currentMatch.home_team.code;
+ const awayFlagId = currentMatch.away_team.code;
+ getFlagApi(homeFlagId, )
+ renderAllStats(currentMatch);
 }
 
-function renderListItem(item) {
+function findMostRecentGame(game) {
+ return game.status !== "future"
+}
+
+function renderHomeStats(item) {
+  return `<div class="centered-text">
+            <p>Goals scored: ${item.home_team.goals}</p>
+          </div> `
+}
+
+function renderAwayStats(item) {
+  return `<div class="centered-text">
+            <p>Goals scored: ${item.away_team.goals}</p>
+          </div> `
+}
+
+function renderMatchInfo(item) {
+  return `<div class="centered-text">
+            <h2>${item.home_team_country}  vs  ${item.away_team_country}</h2>
+          </div>  `
+}
+
+function renderAllStats(item) {
+  $('.left-main').append(renderHomeStats(item));
+  $('.right-main').append(renderAwayStats(item));
+  $('.main-content').append(renderMatchInfo(item));
+}
+
+function getFlagApi(searchTerm, callback){
+  const params = {url: `${flagUrl} + ${searchTerm}` , data: {}, success: callback}
+}
+
+function getHomeFlagPicture(data) {
   return 
 }
-
-
 
 
 
