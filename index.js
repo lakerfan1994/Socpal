b7bebef4a9891c81ab8553b9be7a1d88e2299782
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2018-07-05 15:35:55 -0400

    added flags api and flags to the program

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const todayMatchApi = "https://worldcup.sfg.io/matches";
-const flagUrl = 'https://restcountries.eu/rest/v2/alpha/';
+const flagUrl = 'https://restcountries.eu/rest/v2/name/';
 
 function showCurrentGames(){
   $('.current-game-button').click(function(){
-    console.log("print");
     $('header').addClass("hidden");
     $('.current-game').removeClass('hidden');
     getSoccerApi(updateInfo);
@@ -19,10 +18,10 @@ function getSoccerApi(callback) {
 function updateInfo(data){
   let dataApi = data;
  const currentMatch = data.find(findMostRecentGame);
- const homeFlagId = currentMatch.home_team.code;
- const awayFlagId = currentMatch.away_team.code;
- getFlagApi(homeFlagId, )
+ const homeFlagId = currentMatch.home_team_country;
+ const awayFlagId = currentMatch.away_team_country;
  renderAllStats(currentMatch);
+ getFlagApi(homeFlagId, awayFlagId, renderHomeFlag, renderAwayFlag )
 }
 
 function findMostRecentGame(game) {
@@ -53,12 +52,40 @@ function renderAllStats(item) {
   $('.main-content').append(renderMatchInfo(item));
 }
 
-function getFlagApi(searchTerm, callback){
-  const params = {url: `${flagUrl} + ${searchTerm}` , data: {}, success: callback}
+function getFlagApi(homeFlag, awayFlag, homeCallBack, awayCallBack){
+  if(homeFlag === "England") {
+    homeFlag = "United Kingdom";
+  }
+
+  if(awayFlag === "England") {
+    awayFlag = "United Kingdom";
+  }
+
+  if(homeFlag === "Korea Republic") {
+    homeFlag = "Korea";
+  }
+
+  if(awayFlag === "Korea Republic") {
+    awayFlag = "Korea";
+  }
+  const params = {url: `${flagUrl}${homeFlag}`, success: homeCallBack};
+  console.log(`${flagUrl}${homeFlag}`)
+  $.ajax(params);
+  const params2 = {url: `${flagUrl}${awayFlag}`, success: awayCallBack};
+  console.log(`${flagUrl}${awayFlag}`);
+  $.ajax(params2);
 }
 
-function getHomeFlagPicture(data) {
-  return 
+function getFlagPicture(data) {
+  return `<img src=${data[0].flag}>`
+}
+
+function renderHomeFlag(homeFlag) {
+  $('.flag-1').append(getFlagPicture(homeFlag));
+}
+
+function renderAwayFlag(awayFlag) {
+  $('.flag-2').append(getFlagPicture(awayFlag));
 }
 
 
