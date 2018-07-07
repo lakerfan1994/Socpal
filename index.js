77033b04ec8c636a7c4145cd600e276721eaa0b4
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2018-07-07 18:52:09 -0400

    Implemented search functionality for different teams, also fixed a bunch of small bugs with rest of code

--- index.js
+++ index.js
@@ -3,14 +3,28 @@ const flagUrl = 'https://restcountries.eu/rest/v2/name/';
 const youtubeEmbedder = "https://www.youtube.com/embed/";
 const youtubeEndpoint = "https://www.googleapis.com/youtube/v3/search";
 const keyCode = "AIzaSyDh3vRFgUTk0R12FpgTN-_K11udH4RmHjk";
+const teamListApi = 'https://worldcup.sfg.io/teams/';
+
+function listOfWorldCupTeams(){
+  $('.prev-game-button').click(function(){
+    $('.game-chooser').empty();
+    $('.leading-content').append(`<div class="centered-text"><ul class="list-of-teams"></ul></div>`);
+    getTeamListApi(listTeams);
+
+
+  })
+}
 
 function showCurrentGames(){
   $('.current-game-button').click(function(){
     $('header').addClass("hidden");
+    $('.game-chooser').addClass('hidden');
     $('.current-game').removeClass('hidden');
     getSoccerApi(updateInfo);
   })
 }
+
+
 
 function returnFromGames() {
   $('.back-button').click(function(){
@@ -23,17 +37,46 @@ function returnFromGames() {
 function searchCountryMatches() {
   $('#js-form').submit(function(event){
     event.preventDefault();
+     $('.game-chooser').empty();
+    $('.game-chooser').removeClass('hidden');
+
     getSoccerApi(filterForSearch);
   })
 }
 
 function selectSearchedTeam() {
   $('.game-chooser').on('click', 'button', function(event) {
+    const userAnswer = $('input:checked').val();
+    if(userAnswer === undefined) {
+     return;
+    }
+    else {
     event.preventDefault();
-    console.log("im printing OVER HERE");
-    
+    $('input:checked').siblings().addClass('selectedAnswer');
+    getSoccerApi(chooseSelectedMatch);
 
+
+    }
+    
   })
+}
+
+function chooseSelectedMatch(data) {
+  $('header').addClass("hidden");
+  $('.game-chooser').addClass('hidden');
+  $('.current-game').removeClass('hidden');
+  const userAnswer = $('.selectedAnswer').text();
+  let cleanedString = userAnswer.replace(/vs|0|1|2|3|4|5|6|7|8|9|-/gi, "");
+  let arrayOfTeams = cleanedString.trim().split(" ");
+  const selectedMatch = data.find(item => item.home_team_country === arrayOfTeams[0] && item.away_team_country === arrayOfTeams[2]);
+  const homeFlagId = selectedMatch.home_team_country;
+  const awayFlagId = selectedMatch.away_team_country;
+  const searchMatch = `${homeFlagId} vs ${awayFlagId} fifa world cup 2018`;
+  getYoutubeApi(searchMatch, updateVideos);
+  renderAllStats(selectedMatch);
+  getFlagApi(homeFlagId, awayFlagId, renderHomeFlag, renderAwayFlag );
+
+
 }
 
 
@@ -68,6 +111,11 @@ function getFlagApi(homeFlag, awayFlag, homeCallBack, awayCallBack){
 
 function getYoutubeApi(searchTerm, callback) {
   const params = {url: youtubeEndpoint, data: {part: "snippet", q: searchTerm, key: keyCode}, success: callback};
+  $.ajax(params);
+}
+
+function getTeamListApi(callback) {
+  const params = {url: teamListApi, data: {by_date: "desc"}, success: callback }
   $.ajax(params);
 }
 
@@ -151,23 +199,41 @@ function emptyApp() {
 function filterForSearch(data) {
   let userInput = $('#textfield').val();
   $('#textfield').val('');
-  $('.game-chooser').empty();
   $('.game-chooser').append(`
         <h2 class="centered-text">Which game would you like to view?</h2>
         <form class= "chooser-form centered-text col-6" aria-live="assertive">
           <button type="submit" class="centered-text">Submit</button>
         </form> `);
   const arrayOfMatches = data.filter(item => item.home_team_country === userInput || item.away_team_country === userInput);
+
+  if(arrayOfMatches.length >= 1){
   const renderedMatches = arrayOfMatches.map(renderSearchMatches);
   $('.chooser-form').prepend(renderedMatches);
-  
+  }
+  else{
+    $('.game-chooser').empty();
+    $('.game-chooser').append(`<h2 class="centered-text">Sorry, this entry was invalid. Please enter the full name of the country. Feel free to look up the countries available by clicking on the List of World Cup Teams button above!</h2>`);
+  }
 }
 
 function renderSearchMatches(item) {
   return `<div class="answerOption">
-           <input type="radio" role="radio" required>
-           <span>${item.home_team_country} vs ${item.away_team_country} ${item.home_team.goals} - ${item.away_team.goals}</span>
+           <input type="radio" role="radio" name="answerOption" required>
+           <p>${item.home_team_country} vs ${item.away_team_country} ${item.home_team.goals} - ${item.away_team.goals}</p>
           </div> `
+}
+
+function listTeams(data){
+  console.log(data)
+  const listOfTeams = data.map(item => renderTeamList(item));
+  $('.list-of-teams').prepend(listOfTeams);
+  $('header').addClass('hidden');
+  $('.current-game').removeClass('hidden');
+
+}
+
+function renderTeamList(data){
+  return `<li>${data.country}</li>`;
 }
 
 
@@ -176,3 +242,4 @@ $(showCurrentGames);
 $(returnFromGames);
 $(searchCountryMatches);
 $(selectSearchedTeam);
+$(listOfWorldCupTeams);
